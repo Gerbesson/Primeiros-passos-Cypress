0694a8f58918868172fa0b1431699e2aa86bcb83
Gerbesson Silva <gerbesson_best@hotmail.com>
2025-07-29 15:33:00 -0300

    Added selectors list for login spec

--- cypress/e2e/spec2orange.cy.js
+++ cypress/e2e/spec2orange.cy.js
@@ -1,18 +1,29 @@
 describe('template spec', () => {
-  it .skip('login - sucessed', () => {
+
+  const selectorsList = {
+    usarnameField:"[name='username']",
+    passwordField:"[name='password']",
+    loginButton:  "[type='submit']",
+    sectionTitleTopBar:".oxd-topbar-header-breadcrumb-module",
+    WrongCredentialAlert: "[role='alert']",
+
+
+  }
+
+  it ('login - sucessed', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input') .type('Admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input') .type('admin123')
-    cy.get('.oxd-button') .click()
+    cy.get(selectorsList.usarnameField) .type('Admin')
+    cy.get(selectorsList.passwordField) .type('admin123')
+    cy.get(selectorsList.loginButton) .click()
     cy.location('pathname').should('equal', "/web/index.php/dashboard/index")
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
   })
   it('login - fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input') .type('senha')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input') .type('senha')
-    cy.get('.oxd-button') .click()
-    cy.get('.oxd-alert-content')
+    cy.get(selectorsList.usarnameField).type('Test')
+    cy.get(selectorsList.passwordField).type('Test')
+    cy.get(selectorsList.loginButton) .click()
+    cy.get(selectorsList.WrongCredentialAlert)
     
   })
 })
